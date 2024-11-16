5c9938ba686766438a241003c4bee3eff3e4558c
DESKTOP-HM590JA\USER <meatmania@daum.net>
2024-11-16 20:19:36 +0900

    [meatmania] UI 적용

---
+++ Client/Assets/__Script/UI/Game/UIGamePanel.cs
@@ -0,0 +1,52 @@
+using Game;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGamePanel : MonoBehaviour
+{
+    [Header("Player")]
+    public Slider _playerHpSlider;
+    public Slider _playerSpSlider;
+
+    [Header("Enemy")]
+    public Slider _enemyHPSlider;
+    public Text _enemyName;
+
+    [Space(10)]
+    public HeroBrain _heroBrain;
+    public PawnBrainController _targetPawn;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (_heroBrain == null) 
+        {
+            _heroBrain = FindAnyObjectByType<HeroBrain>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // HP Value
+        var maxValue = _heroBrain.PawnBB.stat.maxHeartPoint.Value;
+        _playerHpSlider.value = (maxValue > 0) ? _heroBrain.PawnHP.heartPoint.Value / maxValue : 0;
+
+        // Stamina Value
+        var maxStamina = _heroBrain.PawnBB.stat.maxStamina.Value;
+        _playerSpSlider.value = (maxStamina > 0) ? _heroBrain.PawnBB.stat.stamina.Value / maxStamina : 0;
+
+        // Enemy HP Value
+        _targetPawn = _heroBrain.BB.TargetBrain;
+        if (_targetPawn != null)
+        {
+            var maxValue2 = _targetPawn.PawnBB.stat.maxHeartPoint.Value;
+            _enemyHPSlider.value = (maxValue > 0) ? _targetPawn.PawnHP.heartPoint.Value / maxValue2 : 0;
+        }
+        else 
+        {
+            _enemyHPSlider.value = 0;
+        }
+    }
+}
