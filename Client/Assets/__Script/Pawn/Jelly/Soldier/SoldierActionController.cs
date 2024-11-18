f50cc3ac5b1921c15fe49c7a633b7e1d06459e35
DESKTOP-HM590JA\USER <meatmania@daum.net>
2024-11-19 00:57:32 +0900

    [meatmania] 공격 이펙트 변경, 강공격 대시 거리 변경 등등

--- Client/Assets/__Script/Pawn/Jelly/Soldier/SoldierActionController.cs
+++ Client/Assets/__Script/Pawn/Jelly/Soldier/SoldierActionController.cs
@@ -79,13 +79,15 @@ namespace Game
                 SoundManager.Instance.Play(SoundID.HIT_BLOCK);
                 EffectManager.Instance.Show("@Hit 4 yellow arrow", __brain.AnimCtrler.shieldMeshSlot.position, Quaternion.identity, Vector3.one, 1f);
             }
+            // Guard Break
             else if (damageContext.actionResult == ActionResults.GuardBreak) 
             {
                 __brain.AnimCtrler.mainAnimator.SetInteger("HitType", 2);
                 __brain.AnimCtrler.mainAnimator.SetTrigger("OnHit");
 
                 SoundManager.Instance.Play(SoundID.GUARD_BREAK);
-                EffectManager.Instance.Show("@Hit 4 yellow arrow", __brain.AnimCtrler.shieldMeshSlot.position, Quaternion.identity, Vector3.one, 1f);
+                EffectManager.Instance.Show("Crash", 
+                    __brain.AnimCtrler.shieldMeshSlot.position, Quaternion.identity, Vector3.one, 1f);
             }
 
             var knockBackDisposable = Observable.EveryUpdate().TakeUntil(Observable.Timer(TimeSpan.FromSeconds(0.2f)))
