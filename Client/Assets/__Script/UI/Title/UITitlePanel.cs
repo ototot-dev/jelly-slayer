497a1c46641f0457a5ee2d28e3afb9d32eaeaa1e
meatmania <meatmania@daum.net>
2025-01-11 19:18:49 +0900

    [meatmania] Title

--- Client/Assets/__Script/UI/Title/UITitlePanel.cs
+++ Client/Assets/__Script/UI/Title/UITitlePanel.cs
@@ -6,6 +6,10 @@ using Game;
 
 public class UITitlePanel : MonoBehaviour
 {
+    int _cursorIndex = 0;
+    public Transform _trCursor;
+    public Transform[] _trButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,15 +17,52 @@ public class UITitlePanel : MonoBehaviour
     }
     public void OnClickGameStart()
     {
-        SceneManager.LoadScene("BattleTest");
+        SetCursorIndex(0);
+        Invoke("DoAction", 0.1f);
     }
     public void OnClickTutorial()
     {
-        SceneManager.LoadScene("BattleTest");
+        SetCursorIndex(1);
+        Invoke("DoAction", 0.1f);
     }
-
     public void OnClickGameExit()
     {
-        Application.Quit();
+        SetCursorIndex(2);
+        Invoke("DoAction", 0.1f);
+    }
+    void SetCursorIndex(int index) 
+    {
+        if(index < 0)
+            index = 0;
+        if (index >= 3)
+            index = 2;
+
+        _cursorIndex = index;
+        _trCursor.position = _trButton[index].position;
+    }
+
+    void DoAction() 
+    {
+        switch (_cursorIndex)
+        {
+            case 0: SceneManager.LoadScene("BattleTest"); break;
+            case 1: SceneManager.LoadScene("BattleTest"); break;
+            case 2: Application.Quit(); break;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SetCursorIndex(_cursorIndex - 1);
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SetCursorIndex(_cursorIndex + 1);
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            DoAction();
+        }
     }
 }
