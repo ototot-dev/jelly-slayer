63a92796e6f9d95116ae8777f1de9002d6205da4
ototot-dev <ototot.dev@gmail.com>
2025-03-21 12:37:07 +0900

    Update HeroActionController.cs

--- Client/Assets/__Script/Pawn/Hero/HeroActionController.cs
+++ Client/Assets/__Script/Pawn/Hero/HeroActionController.cs
@@ -37,7 +37,7 @@ namespace Game
                 return false;
             if (!__brain.SensorCtrler.WatchingColliders.Contains(damageContext.senderBrain.coreColliderHelper.pawnCollider))
                 return false;
-            if (damageContext.receiverBrain.PawnBB.stat.stamina.Value < damageContext.senderActionData.guardStaminaDamage)
+            if (damageContext.receiverBrain.PawnBB.stat.stamina.Value <= 0)
                 return false;
 
             return true;
