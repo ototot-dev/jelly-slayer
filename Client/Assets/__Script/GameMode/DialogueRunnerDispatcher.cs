cd0e34d653ab13b496430df673c429e6bd5f33b8
meatmania <meatmania@daum.net>
2025-05-21 13:26:30 +0900

    [meatmania] 튜토리얼 스크립트 변경

--- Client/Assets/__Script/GameMode/DialogueRunnerDispatcher.cs
+++ Client/Assets/__Script/GameMode/DialogueRunnerDispatcher.cs
@@ -9,7 +9,7 @@ using Yarn.Unity;
 
 namespace Game
 {
-    public class DialogueRunnerDispatcher : DialogueViewBase
+    public partial class DialogueRunnerDispatcher : DialogueViewBase
     {
         public Action onDialoqueStarted;
         public Action<LocalizedLine> onRunLine;
@@ -137,52 +137,6 @@ namespace Game
         public IEnumerator WaitForSeconds(float seconds)
         {
             yield return new WaitForSeconds(seconds);
-        }
-
-        public void JumpTo(string text)
-        {
-            Debug.Log($"From {text}");
-        }
-
-        public void Sleep()
-        {
-            var slayerBrain = GameContext.Instance.playerCtrler.possessedBrain;
-            slayerBrain.GetComponent<SlayerAnimController>().PlaySingleClipLooping(slayerBrain.BB.dialogue.sleepAnimClip, 0.1f);
-        }
-
-        public void GetUp()
-        {
-            var slayerBrain = GameContext.Instance.playerCtrler.possessedBrain;
-            slayerBrain.GetComponent<SlayerAnimController>().PlaySingleClip(slayerBrain.BB.dialogue.getUpAnimClip, 0.5f, 0.5f);
-        }
-
-        public void ShowMechArm()
-        {
-            var slayerBrain = GameContext.Instance.playerCtrler.possessedBrain;
-            slayerBrain.PartsCtrler.SetLeftMechArmHidden(false);
-        }
-
-        public void HideMechArm()
-        {
-            var slayerBrain = GameContext.Instance.playerCtrler.possessedBrain;
-            slayerBrain.PartsCtrler.SetLeftMechArmHidden(true);
-        }
-
-        public void ShowSword()
-        {
-            var slayerBrain = GameContext.Instance.playerCtrler.possessedBrain;
-            slayerBrain.PartsCtrler.SetSwordHidden(false);
-        }
-
-        public void HideSword()
-        {
-            var slayerBrain = GameContext.Instance.playerCtrler.possessedBrain;
-            slayerBrain.PartsCtrler.SetSwordHidden(true);
-        }
-
-        public void PlaySound(string soundName) 
-        {
-            Debug.Log("YarnCommand PlaySound: " + soundName);
         }
     }
 }
---
+++ Client/Assets/__Script/GameMode/DialogueRunnerDispatcher_Tutorial.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using FinalFactory;
+using UniRx;
+using UnityEngine;
+using Yarn.Unity;
+
+namespace Game
+{
+    // Tutorial
+    public partial class DialogueRunnerDispatcher : DialogueViewBase
+    {
+        public void JumpTo(string text)
+        {
+            Debug.Log($"From {text}");
+        }
+
+        public void Sleep()
+        {
+            var slayerBrain = GameContext.Instance.playerCtrler.possessedBrain;
+            slayerBrain.GetComponent<SlayerAnimController>().PlaySingleClipLooping(slayerBrain.BB.dialogue.sleepAnimClip, 0.1f);
+        }
+
+        public void GetUp()
+        {
+            var slayerBrain = GameContext.Instance.playerCtrler.possessedBrain;
+            slayerBrain.GetComponent<SlayerAnimController>().PlaySingleClip(slayerBrain.BB.dialogue.getUpAnimClip, 0.5f, 0.5f);
+        }
+
+        public void ShowMechArm()
+        {
+            var slayerBrain = GameContext.Instance.playerCtrler.possessedBrain;
+            slayerBrain.PartsCtrler.SetLeftMechArmHidden(false);
+        }
+
+        public void HideMechArm()
+        {
+            var slayerBrain = GameContext.Instance.playerCtrler.possessedBrain;
+            slayerBrain.PartsCtrler.SetLeftMechArmHidden(true);
+        }
+
+        public void ShowSword()
+        {
+            var slayerBrain = GameContext.Instance.playerCtrler.possessedBrain;
+            slayerBrain.PartsCtrler.SetSwordHidden(false);
+        }
+
+        public void HideSword()
+        {
+            var slayerBrain = GameContext.Instance.playerCtrler.possessedBrain;
+            slayerBrain.PartsCtrler.SetSwordHidden(true);
+        }
+
+        public void PlaySound(string soundName) 
+        {
+            Debug.Log("YarnCommand PlaySound: " + soundName);
+        }
+    }
+}
