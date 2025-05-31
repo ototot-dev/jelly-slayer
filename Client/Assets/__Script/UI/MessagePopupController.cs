518c1d5bdeb27221adbca3df8029b4fb6c7436f9
meatmania <meatmania@daum.net>
2025-05-31 14:38:47 +0900

    [meatmania] yarn 스크립트 추가

--- Client/Assets/__Script/UI/MessagePopupController.cs
+++ Client/Assets/__Script/UI/MessagePopupController.cs
@@ -17,6 +17,9 @@ namespace Game
         string _text;
         float _showDuration;
 
+        BoolReactiveProperty visibilityFlag = new();
+
+
         public MessagePopupController(string title, string text, float showDuration = 10f) 
         {
             _title = title;
